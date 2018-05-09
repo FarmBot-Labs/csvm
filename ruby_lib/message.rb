b652521301cee73897574360626b3cad9223c69e
Rick Carlino <rick.carlino@gmail.com>
2018-05-09 11:05:48 -0500

    Initializer works, needs validations (stubbed tests)

--- ruby_lib/message.rb
+++ ruby_lib/message.rb
@@ -1,22 +1,60 @@
+# Datastructure to encapsulate a single request header, as defined in the spec.
+# Purpose: Eliminates repetition of common parsing operations, such as
+# extracting header names or parsing uint16's into Ruby number types.
 class Message
   attr_reader :input
   class TooShort < Exception; end
+  class BadSegName < Exception; end
 
-  COMMAND_WIDTH   = 5
-  COMMAND_PADDING = "_"
+  # Declares the name, width, and starting index of a segment within a request
+  # header. See: specification.md for an overview of segments.
+  Segment         = Struct.new(:name, :start, :width)
+  # Every segment in the sepc, indexed by name.
+  SEGMENTS        = [ Segment.new(:CHANNEL,            0,  2),
+                      Segment.new(:NAMESPACE,          2,  4),
+                      Segment.new(:OPERATION,          6,  5),
+                      Segment.new(:PAYLOAD_SIZE_DECLR, 11, 2),
+                      Segment.new(:CLRF,               13, 2) ]
+                    .map do |s| {s.name => s} end
+                    .reduce(:merge)
+  # Character used to pad command/namespace segments when the name is smaller
+  # than the specified segment size.
+  PAD_CHAR        = "_"
+  CRLF            = "\r\n"
+  # Used by String#unpack() to unpack unsigned 16 bit integers (big endian)
+  UINT16          = "S"
+  # The size of a request header, excluding the (flexible) payload.
+  HEADER_SIZE     = SEGMENTS.values.map { |x| x.width }.reduce(:+)
 
-  def self.command_list(*commands)
-    commands.map do |op|
-      op.ljust(COMMAND_WIDTH, COMMAND_PADDING)
-    end
+  def self.command_list(*commands) # Pad/shrinktruncate opnames to correct size.
+    width = SEGMENTS[:OPERATION].width
+    commands.map { |op| op.ljust(width, PAD_CHAR)[0, width] }
   end
 
+  # List of valid "operation" segment values,
+  # indexed under the respective "namespace"
   OPERATIONS = {"CODE" => command_list("NEW", "OPEN", "WRITE", "CLOSE", "RM"),
                 "PROC" => command_list("START", "PAUSE", "KILL", "RUN")}
 
-  attr_reader :channel_id, :payload, :namespace, :operation, :payload
+  attr_reader :input, # Raw input string, as provided by input stream (usually)
+              :channel,
+              :namespace,
+              :operation,
+              :payload_size,
+              :payload
 
   def initialize(input)
+    @input        = input
+    # Prevents NPEs in code below:
+    raise TooShort if @input.length < HEADER_SIZE
+    @channel      = input[segm(:CHANNEL).start, segm(:CHANNEL).width]
+                    .unpack(UINT16)
+                    .first
+    @namespace    = input[segm(:NAMESPACE).start, segm(:NAMESPACE).width]
+    @operation    = input[segm(:OPERATION).start, segm(:OPERATION).width]
+    @payload_size = input[segm(:PAYLOAD_SIZE_DECLR).start,
+                          segm(:PAYLOAD_SIZE_DECLR).width].unpack(UINT16).first
+    @payload      = input[HEADER_SIZE..-1][0, @payload_size]
   end
 
   # Validates syntax but not semantics.
@@ -25,7 +63,13 @@ class Message
 
 private
 
+  # Throw runtime error on malformed segment names (catch typos in test suite)
+  def segm(name)
+    SEGMENTS[name] or raise BadSegName, name
+  end
 end
+
+# TESTS ==================
 
 if RUBY_ENGINE == "ruby"
   require "test-unit"
@@ -43,22 +87,46 @@ if RUBY_ENGINE == "ruby"
     end
 
     def test_too_short
-      pend("Ready to implement")
       assert_raise(Message::TooShort) { Message.new("X").validate! }
     end
 
-    def test_attributes
-      pend("Ready to implement")
+    def test_channel_validation
+      pend("TODO")
+    end
+
+    def test_namespace_validation
+      pend("TODO")
+    end
+
+    def test_operation_validation
+      pend("TODO")
+    end
+
+    def test_payload_size_validation
+      pend("TODO")
+    end
+
+    def test_payload_validation
+      pend("TODO")
+    end
+
+    def test_payl_attributes
+      pend("TODO")
       chan_id      = random_uint16
       payload_size = random_uint16
       namespace    = Message::OPERATIONS.keys.sample
       operation    = Message::OPERATIONS[namespace].sample
       payload      = random_garbage(payload_size.unpack(UINT16).first)
-      input        = \
-        chan_id + payload_size + namespace + operation + payload + input
+      input        = [chan_id,
+                      namespace,
+                      operation,
+                      payload_size,
+                      Message::CRLF,
+                      payload].join("")
       msg          = Message.new(input)
       msg.validate!
-      assert_equal(msg.channel_id,     chan_id.unpack(UINT16))
+
+      assert_equal(msg.channel,     chan_id.unpack(UINT16))
       assert_equal(msg.payload.length, payload_size.unpack(UINT16))
       assert_equal(msg.namespace,      namespace)
       assert_equal(msg.operation,      operation)
