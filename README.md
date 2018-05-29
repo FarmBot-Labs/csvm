cc4f36ba628042e3a1d0d68dabe99b2c694ff27b
Rick Carlino <rick.carlino@gmail.com>
2018-05-29 18:04:02 -0500

    :tada: (rudimentary) coverage reporter

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
+# Deps
+
+```
+sudo luarocks install busted
+sudo luarocks install luacov
+sudo luarocks install penlight
+```
+
 # Run Tests
+
+```
+busted .
+```
+
+# View Coverage
+
+```
+luacov
+cat luacov.report.out
+```
--- lua_lib/inbox_test.lua
+++ lua_lib/inbox_test.lua
@@ -1,2 +1,13 @@
 local inbox = require "lua_lib/inbox"
 local test = require "pl.test"
+describe(
+  "Busted",
+  function()
+    it(
+      "kind of looks like rspec",
+      function()
+        assert.are.same({table = "great"}, {table = "no!"})
+      end
+    )
+  end
+)
