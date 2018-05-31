7b2aa07aa24ed66ee631fbc92132f93918bb6727
Rick Carlino <rick.carlino@gmail.com>
2018-05-31 15:47:58 -0500

    Test for is_function() assertions

---
+++ lua_lib/util/type_assertion_test.lua
@@ -0,0 +1,13 @@
+local T    = require("lua_lib/util/type_assertion")
+
+describe("assertions", function ()
+  it("detects functions", function ()
+    local fn = function ()
+      -- NOOP
+    end
+    assert.has.errors(function() T.is_function(9) end)
+    assert.has_no.errors(function() T.is_function(fn) end)
+    assert.has_no.errors(function() T.is_function(spy.new(fn)) end)
+    assert.has_no.errors(function() T.maybe_function(nil) end)
+  end)
+end)
