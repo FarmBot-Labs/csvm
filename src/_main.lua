918f9372527fff0bfe701fbc2de26bd59fa2d857
Rick Carlino <rick.carlino@gmail.com>
2018-06-04 15:50:58 -0500

    Slicer, first pass. Needs tests

--- src/_main.lua
+++ src/_main.lua
@@ -2,7 +2,6 @@ local inbox      = require("src/io/inbox")
 local reply      = require("src/io/outbox").reply
 local hypervisor = require("src/hypervisor").new(reply)
 local app        = require("src/app")
-X                = require("src/slicer/heap").new()
 
 if (os.getenv("CELERY_ENV") == "dev") then
   inbox.setup_local_dev()
--- src/slicer/slicer.lua
+++ src/slicer/slicer.lua
@@ -4,14 +4,14 @@ local M      = {}
 
 function M.new ()
   local this = {}
+
   this.run = function(node)
     T.is_table(node)
-    this.nesting_level = 0
     this.root_node     = node
     local heap = CSHeap.new()
     this.allocate(heap, node, CSHeap.NULL)
-    this.heap_values = heap.values()
-    this.heap_values.map(function (x)
+    heap = heap.entries
+    heap:map(function (x)
       if not x[CSHeap.BODY] then
         x[CSHeap.BODY] = CSHeap.NULL
       end
@@ -19,69 +19,68 @@ function M.new ()
         x[CSHeap.NEXT] = CSHeap.NULL
       end
     end)
-    heap.entries()
+    return heap.entries
   end
 
-  --   attr_reader :root_node
+  this.recurse_into_body =
+    function(heap, canonical_list, previous_address, i)
+      local index = i or 0
+      if canonical_list[index] then
+        local is_head = index == 0
+        if is_head then
+          -- BE CAREFUL EDITING THIS LINE, YOU MIGHT BREAK `BODY` NODES:
+          heap.put(previous_address,
+                   CSHeap.BODY,
+                   previous_address + 1) -- See note above!
+        end
+
+        local my_heap_address = this.allocate(heap,
+                                              canonical_list[index],
+                                              previous_address)
+        if is_head then
+          heap.put(previous_address, CSHeap.NEXT, CSHeap.NULL)
+        else
+          heap.put(previous_address, CSHeap.NEXT, my_heap_address)
+        end
+
+        this.recurse_into_body(heap, canonical_list, my_heap_address, index + 1)
+      end
+    end
+
+  this.is_celery_script = function(node)
+    return (type(node) == "table") and (node.args) and (node.kind)
+  end
+
+  this.iterate_over_body = function(heap, canonical_node, parentAddr)
+    local body = (canonical_node.body or {})
+    this.recurse_into_body(heap, body, parentAddr)
+  end
+
+  this.iterate_over_args = function(h, s, parentAddr)
+    local tbl = s.args or {}
+    for key, _ in pairs(tbl) do
+      local v = s.args[key]
+      if this.is_celery_script(v) then
+        local k = CSHeap.LINK + key.to_s
+        h.put(parentAddr, k, this.allocate(h, v, parentAddr))
+      else
+        h.put(parentAddr, key, v)
+      end
+    end
+   end
+
+  this.allocate = function (h, s, parentAddr)
+    local addr = h.allot(s.kind)
+    h.put(addr, CSHeap.PARENT, parentAddr)
+    if s.comment then
+      h.put(addr, CSHeap.COMMENT, s.comment)
+    end
+    this.iterate_over_body(h, s, addr)
+    this.iterate_over_args(h, s, addr)
+    return addr
+  end
+
   return this
 end
 
 return M
---   class Slicer
-
---   def is_celery_script(node)
---     node && node.is_a?(Hash) && node[:args] && node[:kind]
---   end
-
---   def heap_values
---     this.heap_values
---   end
-
---   def allocate(h, s, parentAddr)
---     addr = h.allot(s[:kind])
---     h.put(addr, CSHeap.PARENT, parentAddr)
---     h.put(addr, CSHeap.COMMENT, s[:comment]) if s[:comment]
---     iterate_over_body(h, s, addr)
---     iterate_over_args(h, s, addr)
---     addr
---   end
-
---   def iterate_over_args(h, s, parentAddr)
---     (s[:args] || {})
---     .keys
---     .map do |key|
---       v = s[:args][key]
---       if (is_celery_script(v))
---       k = CSHeap.LINK + key.to_s
---       h.put(parentAddr, k, allocate(h, v, parentAddr))
---       else
---       h.put(parentAddr, key, v)
---       end
---     end
---   end
-
---   def iterate_over_body(heap, canonical_node, parentAddr)
---     body = (canonical_node[:body] || []).map(&:deep_symbolize_keys)
---     # !body.none? && heap.put(parentAddr, CSHeap.BODY, parentAddr + 1)
---     this.nesting_level += 1
---     recurse_into_body(heap, body, parentAddr)
---     this.nesting_level -= 1
---   end
-
---   def recurse_into_body(heap, canonical_list, previous_address, index = 0)
---     if canonical_list[index]
---     is_head     = index == 0
---     # BE CAREFUL EDITING THIS LINE, YOU MIGHT BREAK `BODY` NODES:
---     heap # See note above!
---       .put(previous_address, CSHeap.BODY, previous_address + 1) if is_head
-
---     my_heap_address = allocate(heap, canonical_list[index], previous_address)
-
---     prev_next_key = is_head ? CSHeap.NULL : my_heap_address
---     heap.put(previous_address, CSHeap.NEXT, prev_next_key)
-
---     recurse_into_body(heap, canonical_list, my_heap_address, index + 1)
---     end
---   end
---   end
--- end
