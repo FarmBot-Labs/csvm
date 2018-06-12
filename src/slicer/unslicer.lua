b7734703fcae0f63b3780c3c85c14e4d838f40e7
Rick Carlino <rick.carlino@gmail.com>
2018-06-12 16:22:20 -0500

    Unslices edge_nodes :ok:

--- src/slicer/unslicer.lua
+++ src/slicer/unslicer.lua
@@ -1,19 +1,35 @@
 local Heap    = require("src/slicer/heap")
--- local inspect = require("pl.pretty")
+local inspect = require("pl.pretty")
 local List    = require("pl.List")
 local M       = {}
 
--- local handlers = {
---   [M.PARENT]  = function() end,
---   [M.BODY]    = function() end,
---   [M.NEXT]    = function() end,
---   [M.KIND]    = function() end,
---   [M.COMMENT] = function() end,
---   [M.NOTHING] = function() end,
--- }
+local handlers = {
+  [Heap.BODY]    = function(heap, _, output, value)
+    output.body      = List.new()
+    local next_addr  = value
+    local n          = heap[next_addr]
+    local count      = 0
+    while n and (n[Heap.KIND] ~= "nothing") do
+      count = count + 1
+      if (count > 1000) then error("Runaway loop") end
+      local item = M.unslice(heap, next_addr)
+      output.body.push(item)
+      next_addr = item[Heap.NEXT]
+      n         = heap[next_addr]
+    end
+  end,
+  [Heap.KIND]    = function(heap, address, output, value)
+    output.kind = value
+  end,
+  [Heap.PARENT]  = function() end,
+  [Heap.NEXT]    = function() end,
+  [Heap.COMMENT] = function() end,
+  [Heap.NOTHING] = function() end,
+}
 
--- local default_handler = function()
--- end
+local default_handler = function(heap, _, output, value, key)
+  output.args[key:sub(2, -1)] = M.unslice(heap, value)
+end
 
 M.unslice = function (heap, address)
   local output = { args = {} }
@@ -21,29 +37,13 @@ M.unslice = function (heap, address)
 
   for k, value in pairs(here) do
     if (k:sub(1, 2) == "__") then
-      print("KEY: " .. k .. " VALUE: " .. value)
-      if k == Heap.KIND then
-        output.kind = value
-      end
-
-      if k == Heap.BODY then
-        output.body      = List.new()
-        local next_addr  = here[Heap.BODY]
-        local n          = heap[next_addr]
-        local count      = 0
-        while n and (n[Heap.KIND] ~= "nothing") do
-          count = count + 1
-          if (count > 1000) then error("Runaway loop") end
-          local item = M.unslice(heap, next_addr)
-          output.body.push(item)
-          next_addr = item[Heap.NEXT]
-          n         = heap[next_addr]
-        end
-      end
+      local handler = (handlers[k] or default_handler)
+      handler(heap, address, output, value, k)
     else
       output.args[k] = value
     end
   end
+  inspect.dump(output)
   return output
 end
 
