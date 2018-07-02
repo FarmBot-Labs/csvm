016e5cbe2298aabd93f2c4bcb824f5a8862ad963
Rick Carlino <rick.carlino@gmail.com>
2018-07-02 09:53:34 -0500

    :thinking_face:

--- test/csvm/resolver_test.exs
+++ test/csvm/resolver_test.exs
@@ -7,19 +7,18 @@ defmodule Csvm.ResolverTest do
     File.read!(fname)
       |> Jason.decode!()
       |> AST.decode()
-      |> AST.Slicer.run()
   end
 
   test "variable resolution" do
-    fun        = fn _ -> {:error, "whatever"} end
-    inner_json = fetch_fixture("fixture/inner_sequence.json")
-    outer_json = fetch_fixture("fixture/outer_sequence.json")
+    fun = fn x ->
+      IO.inspect(x || "NO")
+      { :ok, fetch_fixture("fixture/inner_sequence.json") }
+    end
+    outer_json = fetch_fixture("fixture/outer_sequence.json") |> AST.Slicer.run()
     farm_proc0 = FarmProc.new(fun, addr(0), outer_json)
-    farm_proc1 = Enum.reduce(0..3, farm_proc0, fn x, acc ->
-      IO.inspect(x)
+    farm_proc1 = Enum.reduce(0..6, farm_proc0, fn _, acc ->
       FarmProc.step(acc)
     end)
-    IO.inspect(farm_proc1)
-    raise "Opps"
+    IO.puts("THIS IS NOT DONE - RC")
   end
 end
